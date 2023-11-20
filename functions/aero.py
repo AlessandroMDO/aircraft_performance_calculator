200a87bbeb67000d33de6e2f7c7d69a37811cb33
Alessandro Melo <alessandromelooliver@gmail.com>
2023-11-20 15:58:55 -0300

    build: general aero functions

--- functions/aero.py
+++ functions/aero.py
@@ -7,17 +7,20 @@ sys.path.append(parent_dir)
 import math
 
 
+class Aero:
 
-class Constants:
-    g = 9.81                                    # Aceleração da gravidade [m/s^2]
-    h_Sc = 15.24                                # Altura mínima de decolagem (screen height)
-    R_gas = 287.058                             # J/(kg*K) Constante dos gases
-    maximum_breaking_constant = 0.55 * 9.81
-    medium_breaking_constant = 0.35 * 9.81
-    minimum_breaking_constant = 0.15 * 9.81
-    # gamma_Ap = np.deg2rad(3)
-    gamma_Ap = math.radians(3)
+    def __init__(self):
 
+        self.R_gas = 287.058  
+        self.g = 9.81
+        self.rho_0 = self.get_density(altitude=0)
+        #self.g = 9.81                       # Aceleração da gravidade [m/s^2]
+        self.h_Sc = 15.24                   # Altura mínima de decolagem (screen height)
+        #self.R_gas = 287.058                # J/(kg*K) Constante dos gases
+        self.maximum_breaking_constant = 0.55 * 9.81
+        self.medium_breaking_constant = 0.35 * 9.81
+        self.minimum_breaking_constant = 0.15 * 9.81
+        self.gamma_Ap = math.radians(3)
 
     def calculate_general_drag_coefficient(self, S, K, CD0, altitude, V, W):
         rho = self.get_density(altitude=altitude)
@@ -28,14 +31,15 @@ class Constants:
 
         return CD
 
-    def calculate_general_thrust(self):
-        T = 1
+    def calculate_general_thrust(self, altitude, thrust_factor, sea_level_thrust):
+
+        T = sea_level_thrust * (self.get_sima(altitude=altitude) ** thrust_factor)
         return T
 
     def cal(self, p0, t0, a, h0, h1):
         if a != 0:
             t1 = t0 + a * (h1 - h0)
-            p1 = p0 * (t1 / t0) ** (-self.g / a / self.R_gas)
+            p1 = p0 * (t1 / t0) ** (-1 * self.g / a / self.R_gas)
         else:
             t1 = t0
             p1 = p0 * math.exp(-self.g / self.R_gas / t0 * (h1 - h0))
@@ -56,9 +60,24 @@ class Constants:
                 temperature, pressure = self.cal(p0, t0, a[i], prevh, altitude)
                 break
             else:
-                # sth like dynamic programming
                 t0, p0 = self.cal(p0, t0, a[i], prevh, h[i])
                 prevh = h[i]
 
         density = pressure / (self.R_gas * temperature)
         return density
+
+
+    def get_sima(self, altitude):
+
+        rho = self.get_density(altitude=altitude)
+
+        return rho / self.rho_0
+
+    def calculate_stall_velocity(self, W, rho, CL_max, S, logger):
+
+        V_S = math.sqrt(2 * W / (CL_max * S * rho))
+
+        logger.debug(f"Stall Velocity: {V_S}")
+
+        return V_S
+
