299b01f90af7cf1a8db6c804a78954ffc09ebacd
Alessandro Melo <alessandromelooliver@gmail.com>
2024-01-06 19:16:28 -0300

    fix: tab bar

--- guis/main_gui.py
+++ guis/main_gui.py
@@ -1,6 +1,8 @@
-from PySide2.QtWidgets import QApplication, QMainWindow, QTabWidget, QPushButton, QWidget, QHBoxLayout, QAction
+from PySide2.QtWidgets import QApplication, QMainWindow, QTabWidget, QPushButton, QWidget, QHBoxLayout, QAction, QTabBar
 from ui_takeoff_landing import GUI_DECOLAGEM
 from ui_aircraft_parameters import GUI_AIRCRAFT_PARAMETERS
+from ui_flight_conditions import GUI_FLIGHT_CONDITIONS
+from PySide2.QtCore import QRect
 
 
 
@@ -13,46 +15,67 @@ class MainWindow(QMainWindow):
         self.setMaximumSize(800, 800)
 
         self.stacked_widget = QTabWidget()
+        self.bar = QTabBar()
 
-        menu_widget = QWidget()
-        menu_layout = QHBoxLayout(menu_widget)
+        self.stacked_widget.setTabBar(self.bar)
+        self.bar.hide()
 
         self.window_actions = {
-            "WINDOW_GUI_AIRCRAFT_PARAMETERS": self.create_gui_aircraft_parameters,
-            "WINDOW_GUI_TAKEOFF_LANDING": self.create_gui_takeoff_landing
+            "WINDOW_GUI_AIRCRAFT_PARAMETERS": {"window_action": self.create_gui_aircraft_parameters, "location": QRect(46, 4, 161, 22)},
+            "WINDOW_GUI_FLIGHT_CONDITIONS": {"window_action": self.create_gui_flight_conditions, "location": QRect(229, 4, 161, 22)},
+            "WINDOW_GUI_TAKEOFF_LANDING": {"window_action": self.create_gui_takeoff_landing, "location": QRect(400, 4, 161, 22)},
         }
 
         # Dictionary to store the state of each window
         self.window_state = {
             "WINDOW_GUI_AIRCRAFT_PARAMETERS": None,
+            "WINDOW_GUI_FLIGHT_CONDITIONS": None,
             "WINDOW_GUI_TAKEOFF_LANDING": None
         }
 
-        # Create menu buttons dynamically
-        for window_name in self.window_actions:
-            action = QAction(window_name, self)
-            action.triggered.connect(self.window_actions[window_name])
-            menu_button = QPushButton(window_name)
-            menu_layout.addWidget(menu_button)
-            menu_button.clicked.connect(action.trigger)
 
-        self.setMenuWidget(menu_widget)
         self.setCentralWidget(self.stacked_widget)
 
         first_window_name = next(iter(self.window_actions.keys()))
-        self.window_actions[first_window_name]()
+        self.window_actions[first_window_name]['window_action']()
+
+    def create_buttons(self, ex):
+
+        for window_name in self.window_actions:
+
+            button_loc = self.window_actions[window_name]['location']
+
+            menu_button = QPushButton(ex)
+            menu_button.setText("")  # Set an empty text to hide the label
+            menu_button.setGeometry(button_loc)
+            menu_button.setStyleSheet("border: none; background: none;")
+            menu_button.clicked.connect(self.window_actions[window_name]['window_action'])
+            menu_button.raise_()
 
     def create_gui_aircraft_parameters(self):
-        # print(self.window_state)
         if self.window_state["WINDOW_GUI_AIRCRAFT_PARAMETERS"] is None:
+
             ex = GUI_AIRCRAFT_PARAMETERS()
             ex.setupUi(ex)
 
-            self.stacked_widget.addTab(ex, "")
+            self.create_buttons(ex=ex)
+            self.stacked_widget.insertTab(0, ex, "")
             self.window_state["WINDOW_GUI_AIRCRAFT_PARAMETERS"] = ex
 
-        # Switch to the existing tab
+
         self.stacked_widget.setCurrentWidget(self.window_state["WINDOW_GUI_AIRCRAFT_PARAMETERS"])
+
+    def create_gui_flight_conditions(self):
+        if self.window_state["WINDOW_GUI_FLIGHT_CONDITIONS"] is None:
+            ex = GUI_FLIGHT_CONDITIONS()
+            ex.setupUi(ex)
+
+            self.create_buttons(ex=ex)
+            self.stacked_widget.insertTab(1, ex, "")
+            self.window_state["WINDOW_GUI_FLIGHT_CONDITIONS"] = ex
+
+
+        self.stacked_widget.setCurrentWidget(self.window_state["WINDOW_GUI_FLIGHT_CONDITIONS"])
 
     def create_gui_takeoff_landing(self):
 
@@ -60,14 +83,17 @@ class MainWindow(QMainWindow):
             ex = GUI_DECOLAGEM(aircraft_parameters_class=self.window_state["WINDOW_GUI_AIRCRAFT_PARAMETERS"])
             ex.setupUi(ex)
 
-            self.stacked_widget.addTab(ex, "")
+            self.create_buttons(ex=ex)
+            self.stacked_widget.insertTab(2, ex, "")
             self.window_state["WINDOW_GUI_TAKEOFF_LANDING"] = ex
 
-        # Switch to the existing tab
+
+
         self.stacked_widget.setCurrentWidget(self.window_state["WINDOW_GUI_TAKEOFF_LANDING"])
 
 
 if __name__ == "__main__":
+
     app = QApplication([])
     window = MainWindow()
     window.show()
