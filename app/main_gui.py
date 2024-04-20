ba2135ecc12989c7af24e93e495c3f5a8d8476b4
Alessandro Melo <alessandromelooliver@gmail.com>
2024-04-20 13:03:18 -0300

    fix: update warning

--- app/main_gui.py
+++ app/main_gui.py
@@ -3,8 +3,13 @@ from guis.ui_aircraft_parameters import GUI_AIRCRAFT_PARAMETERS
 from guis.ui_flight_conditions import GUI_FLIGHT_CONDITIONS
 from guis.ui_results import GUI_RESULTS
 from PySide2.QtCore import QRect
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+from functions.aero import Aero
 
 
+aero = Aero()
 
 
 class MainWindow(QMainWindow):
@@ -20,6 +25,11 @@ class MainWindow(QMainWindow):
         self.setMinimumSize(800, 800)
         self.setMaximumSize(800, 800)
 
+        my_pixmap = QPixmap("gui_icon.png")
+        my_icon = QIcon(my_pixmap)
+
+        self.setWindowIcon(my_icon)
+
         self.stacked_widget = QTabWidget()
         self.bar = QTabBar()
 
@@ -27,23 +37,54 @@ class MainWindow(QMainWindow):
         self.bar.hide()
 
         self.window_actions = {
-            "WINDOW_GUI_AIRCRAFT_PARAMETERS": {"window_action": self.create_gui_aircraft_parameters, "location": QRect(46, 4, 161, 22)},
-            "WINDOW_GUI_FLIGHT_CONDITIONS": {"window_action": self.create_gui_flight_conditions, "location": QRect(229, 4, 161, 22)},
-            "WINDOW_GUI_RESULTS": {"window_action": self.create_gui_results, "location": QRect(400, 4, 161, 22)},
-        }
+            "WINDOW_GUI_AIRCRAFT_PARAMETERS": {"window_action": self.create_gui_aircraft_parameters, "location": QRect(50, 5, 161, 22)},
+            "WINDOW_GUI_FLIGHT_CONDITIONS": {"window_action": self.create_gui_flight_conditions, "location": QRect(316, 5, 161, 22)},
+            "WINDOW_GUI_RESULTS": {"window_action": self.create_gui_results, "location": QRect(578, 5, 161, 22)}}
 
         # Dictionary to store the state of each window
         self.window_state = {
             "WINDOW_GUI_AIRCRAFT_PARAMETERS": None,
             "WINDOW_GUI_FLIGHT_CONDITIONS": None,
-            "WINDOW_GUI_RESULTS": None
-        }
+            "WINDOW_GUI_RESULTS": None}
 
 
         self.setCentralWidget(self.stacked_widget)
 
         first_window_name = next(iter(self.window_actions.keys()))
         self.window_actions[first_window_name]['window_action']()
+
+    def warning_box(self, message, ok_and_decline=False):
+
+        msg_box = QMessageBox()
+        msg_box.setIcon(QMessageBox.Warning)
+        msg_box.setText(message)
+        msg_box.setWindowTitle('Warning')
+
+        if ok_and_decline is False:
+            msg_box.exec_()
+        else:
+            ok_button = QPushButton('Yes')
+            msg_box.addButton(ok_button, QMessageBox.AcceptRole)
+
+            decline_button = QPushButton('No')
+            msg_box.addButton(decline_button, QMessageBox.RejectRole)
+            result = msg_box.exec_()
+
+            return result
+
+    def check_payload(self, flight_parameters, aircraft_parameters):
+
+        NP = flight_parameters['NUMBER_OF_PASSENGERS']  # number of passengers
+        FW = flight_parameters['FUEL_WEIGHT']  # fuel weight
+        CW = flight_parameters['DISPATCHED_CARGO_WEIGHT']
+        OEW = aircraft_parameters['OEW']
+        MTOW = aircraft_parameters['MTOW']
+
+        TOW = float(NP * aero.person_weight + OEW + FW + CW)
+
+        if TOW > MTOW:
+            perc = round(TOW/MTOW, 2)
+            self.warning_box(f"The current weight conguration exceeds MTOW by {perc}%.\nThe results may be incorrent. Please fix it.")
 
     def create_buttons(self, ex):
 
@@ -97,6 +138,7 @@ class MainWindow(QMainWindow):
 
         if self.window_state["WINDOW_GUI_RESULTS"] is None:
 
+            self.check_payload(aircraft_parameters=self.aircraft_parameters, flight_parameters=flight_parameters)
             self.gui_results = GUI_RESULTS(aircraft_parameters=self.aircraft_parameters, flight_parameters=flight_parameters)
             self.gui_results.setupUi(self.gui_results)
             self.gui_results.calculate_all_results()
@@ -108,6 +150,8 @@ class MainWindow(QMainWindow):
         else:
             self.flight_parameters = self.gui_flight_conditions.get_flight_parameters()
             self.aircraft_parameters = self.gui_aircraft_parameters.get_aircraft_parameters(convert_units=True)
+
+            self.check_payload(aircraft_parameters=self.aircraft_parameters, flight_parameters=flight_parameters)
 
             self.gui_results.update_parameters(new_flight_parameters=self.flight_parameters, new_aircraft_parameters=self.aircraft_parameters)
             self.gui_results.calculate_all_results()
@@ -121,3 +165,6 @@ if __name__ == "__main__":
     window = MainWindow()
     window.show()
     app.exec_()
+
+
+#https://tree.nathanfriend.io/?s=(%27op2!(%27fancy3~fullPath!false~trailingSlash3~rootDot3)~7(%277%27app-db*utilsA.db-func2A0climb0gli4Cplot_geo085-guis*6results068_C6flight_condi206aircraft_parameters5-main_gui5-tests**B%27)~version!%271%27)*-9-B905*2tions3!true4ding05.py6ui_7source!8takeoff9%20%20A*aeroB%5CnClan4%01CBA987654320-*
